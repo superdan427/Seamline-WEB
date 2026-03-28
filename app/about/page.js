becd2c9693d27dc5c32551a0af4984bbe7508a1b
superdan427 <xudaniel3@gmail.com>
2026-03-29 00:37:37 +0800

    fix: escape apostrophes in about page

--- app/about/page.js
+++ app/about/page.js
@@ -11,24 +11,24 @@ export default function AboutPage() {
       <main className="about-page">
 
         <p className="about-hook">
-          Scrolling. Comparing. Saving inspiration you'll never open again.
-          It feels like doing something. It isn't.
+          Scrolling. Comparing. Saving inspiration you&apos;ll never open again.
+          It feels like doing something. It isn&apos;t.
         </p>
 
         <p className="about-body">
-          Seamline gets you outside. Find small shops you didn't know existed and feel the materials.
-
+          Seamline gets you outside. Find small shops you didn&apos;t know existed and feel the materials.
           A directory of fabric shops, suppliers, tailors. The places fashion is actually built on.
           Pick a place. Go there. Touch the fabric. Make something.
         </p>
+
         <p className="about-stamp">
           Less content. Less ego. Less gatekeeping.
         </p>
 
         <p className="about-stamp">
-          Know a place we&apos;re missing?{' '}  </p>
-        <p className ="about-submit"> <a href="/submit">Submit it here </a>. </p>
-
+          Know a place we&apos;re missing?{' '}
+          <a href="/submit">Submit it here</a>.
+        </p>
 
       </main>
     </div>
